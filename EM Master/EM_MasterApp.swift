02bd89ce039bab7eb49de553216576cd794f7bdd
Benjamin Böhringer <benjamin_boehringer@icloud.com>
2026-08-28 10:26:38 +0200

    Initial Commit

---
+++ EM Master/EM_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  EM_MasterApp.swift
+//  EM Master
+//
+//  Created by Benjamin Böhringer on 28.08.26.
+//
+
+import SwiftUI
+
+@main
+struct EM_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
